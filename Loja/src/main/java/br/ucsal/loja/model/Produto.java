be2278f5ddab147ccfa44c12ca9859f22c26ab79
Mario Jorge Pereira <mariojp@gmail.com>
2018-10-31 08:46:01 -0300

    Envio Inicial

---
+++ Loja/src/main/java/br/ucsal/loja/model/Produto.java
@@ -0,0 +1,51 @@
+package br.ucsal.loja.model;
+
+public class Produto {
+
+	
+	private Long id;
+	private String name;
+	private String status;
+	private String email;
+	private String description;
+	
+	
+	
+	public Produto(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
+
